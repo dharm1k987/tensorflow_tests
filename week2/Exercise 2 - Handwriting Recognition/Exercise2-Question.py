4a5605599786b020f08155affa613dfece4a0391
= <dharm1k987@gmail.com>
2020-06-24 18:16:50 -0400

    did ex2

---
+++ week2/Exercise 2 - Handwriting Recognition/Exercise2-Question.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        if (logs.get('accuracy') >= 0.99):
+            print('\nReached 99% accuracy so cancelling training!')
+            self.model.stop_training = True
+
+callbacks = myCallback()
+
+
+mnist = keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# normalize the data
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+model = tf.keras.models.Sequential([
+    keras.layers.Flatten(),
+    keras.layers.Dense(512, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
+
+classifications = model.predict(x_test)
+list(map(lambda x: print('{0:.10f}'.format(x)), classifications[0]))
+
+print('The real value is {}'.format(y_test[0]))
