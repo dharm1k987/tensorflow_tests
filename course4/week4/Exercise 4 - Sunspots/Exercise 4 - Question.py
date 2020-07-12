8f20c495b7040902717516d13b417670ef1e8059
= <dharm1k987@gmail.com>
2020-07-12 18:22:23 -0400

    finish exercise, had to change windowed dataset fn

--- course4/week4/Exercise 4 - Sunspots/Exercise 4 - Question.py
+++ course4/week4/Exercise 4 - Sunspots/Exercise 4 - Question.py
@@ -39,14 +39,13 @@ batch_size = 32
 shuffle_buffer_size = 1000
 
 def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
-    # expand dims here since we will not have a lambda layer
     series = tf.expand_dims(series, axis=-1)
-    dataset = tf.data.Dataset.from_tensor_slices(series)
-    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
-    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
-    dataset = dataset.shuffle(shuffle_buffer).map(lambda window: (window[:-1], window[-1]))
-    dataset = dataset.batch(batch_size).prefetch(1)
-    return dataset
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size + 1, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size + 1))
+    ds = ds.shuffle(shuffle_buffer)
+    ds = ds.map(lambda w: (w[:-1], w[1:]))
+    return ds.batch(batch_size).prefetch(1)
 
 # helper method to forecast
 def model_forecast(model, series, window_size):
