89a4bd5aa0eb7eefddeeeb97de16e8d9aaa1a59e
= <dharm1k987@gmail.com>
2020-07-12 16:38:25 -0400

    add lesson 2

--- course4/week4/Lesson 2.py
+++ course4/week4/Lesson 2.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
 def plot_series(time, series, format="-", start=0, end=None):
     plt.plot(time[start:end], series[start:end], format)
@@ -8,44 +9,32 @@ def plot_series(time, series, format="-", start=0, end=None):
     plt.ylabel("Value")
     plt.grid(True)
 
-def trend(time, slope=0):
-    return slope * time
+# read the csv file
+time_step = []
+sunspots = []
 
-def seasonal_pattern(season_time):
-    """Just an arbitrary pattern, you can change it if you wish"""
-    return np.where(season_time < 0.4,
-                    np.cos(season_time * 2 * np.pi),
-                    1 / np.exp(3 * season_time))
+with open('./Sunspots.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    # read the header
+    next(reader)
+    for row in reader:
+        sunspots.append(float(row[2]))
+        time_step.append(float(row[0]))
 
-def seasonality(time, period, amplitude=1, phase=0):
-    """Repeats the same pattern at each period"""
-    season_time = ((time + phase) % period) / period
-    return amplitude * seasonal_pattern(season_time)
+# convert to numpy arrays
+series = np.array(sunspots)
+time = np.array(time_step)
+series = np.array(sunspots)
+time = np.array(time_step)
 
-def noise(time, noise_level=1, seed=None):
-    rnd = np.random.RandomState(seed)
-    return rnd.randn(len(time)) * noise_level
-
-time = np.arange(4 * 365 + 1, dtype="float32")
-baseline = 10
-series = trend(time, 0.1)  
-baseline = 10
-amplitude = 40
-slope = 0.05
-noise_level = 5
-
-# Create the series
-series = baseline + trend(time, slope) + seasonality(time, period=365, amplitude=amplitude)
-# Update with noise
-series += noise(time, noise_level, seed=42)
-
-split_time = 1000
+# split data
+split_time = 3000
 time_train = time[:split_time]
 x_train = series[:split_time]
 time_valid = time[split_time:]
 x_valid = series[split_time:]
 
-window_size = 20
+window_size = 30
 batch_size = 32
 shuffle_buffer_size = 1000
 
@@ -68,57 +57,56 @@ def model_forecast(model, series, window_size):
     forecast = model.predict(ds)
     return forecast
 
-# generate training set
 tf.keras.backend.clear_session()
 tf.random.set_seed(51)
 np.random.seed(51)
+window_size = 64
+batch_size = 256
 
-window_size = 30
-train_set = windowed_dataset(x_train, window_size, batch_size=128, shuffle_buffer=shuffle_buffer_size)
+# get our training set
+train_set = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
 
-# create the model
+# create our model
 model = tf.keras.models.Sequential([
-    # add a conv layer instead of the lambda layer
     tf.keras.layers.Conv1D(filters=32, kernel_size=5,
-                      strides=1, padding="causal",
-                      activation="relu",
-                      input_shape=[None, 1]),
-    # make both LSTM bidirectional
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32, return_sequences=True)),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32, return_sequences=True)),
+                        strides=1, padding="causal",
+                        activation="relu",
+                        input_shape=[None, 1]),
+    tf.keras.layers.LSTM(64, return_sequences=True),
+    tf.keras.layers.LSTM(64, return_sequences=True),
+    tf.keras.layers.Dense(30, activation="relu"),
+    tf.keras.layers.Dense(10, activation="relu"),
     tf.keras.layers.Dense(1),
-    tf.keras.layers.Lambda(lambda x: x * 200)
+    tf.keras.layers.Lambda(lambda x: x * 400)
 ])
 
-# determine an optimal learning rate
+# find a good learning rate
 lr_schedule = tf.keras.callbacks.LearningRateScheduler(
     lambda epoch: 1e-8 * 10**(epoch / 20))
-
 optimizer = tf.keras.optimizers.SGD(lr=1e-8, momentum=0.9)
 # compile the model
 model.compile(loss=tf.keras.losses.Huber(),
               optimizer=optimizer,
               metrics=["mae"])
-
 # fit the model
 history = model.fit(train_set, epochs=100, callbacks=[lr_schedule])
 
 plt.semilogx(history.history["lr"], history.history["loss"])
-plt.axis([1e-8, 1e-4, 0, 30])
+plt.axis([1e-8, 1e-4, 0, 60])
 plt.show()
 
 # compile model again with better learning rate
 model = tf.keras.models.Sequential([
-    # add a conv layer instead of the lambda layer
-    tf.keras.layers.Conv1D(filters=32, kernel_size=3,
-                      strides=1, padding="causal",
-                      activation="relu",
-                      input_shape=[None, 1]),
-    # make both LSTM bidirectional
-    tf.keras.layers.LSTM(32, return_sequences=True),
-    tf.keras.layers.LSTM(32, return_sequences=True),
+    tf.keras.layers.Conv1D(filters=60, kernel_size=5,
+                        strides=1, padding="causal",
+                        activation="relu",
+                        input_shape=[None, 1]),
+    tf.keras.layers.LSTM(60, return_sequences=True),
+    tf.keras.layers.LSTM(60, return_sequences=True),
+    tf.keras.layers.Dense(30, activation="relu"),
+    tf.keras.layers.Dense(10, activation="relu"),
     tf.keras.layers.Dense(1),
-    tf.keras.layers.Lambda(lambda x: x * 200)
+    tf.keras.layers.Lambda(lambda x: x * 400)
 ])
 
 optimizer = tf.keras.optimizers.SGD(lr=1e-5, momentum=0.9)
@@ -127,15 +115,12 @@ model.compile(loss=tf.keras.losses.Huber(),
               optimizer=optimizer,
               metrics=["mae"])
 # fit the model
-history = model.fit(train_set, epochs=500)
+history = model.fit(train_set,epochs=500)
 
-# forecast and plot
+# plot the data
 rnn_forecast = model_forecast(model, series[..., np.newaxis], window_size)
 rnn_forecast = rnn_forecast[split_time - window_size:-1, -1, 0]
-
 plt.figure(figsize=(10, 6))
 plot_series(time_valid, x_valid)
 plot_series(time_valid, rnn_forecast)
 plt.show()
-
-print(tf.keras.metrics.mean_absolute_error(x_valid, rnn_forecast).numpy())
